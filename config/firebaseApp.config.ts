e5b48eb181a4f8e5fe37fe68063ca84de8032c81
Nok <dive877@gmail.com>
2022-11-29 21:47:26 +0400

    Implement firebase ui auth

---
+++ config/firebaseApp.config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAkTlPW3Xx0rvHueQ0wgyed8Zn2d4X3PHU",
+  authDomain: "awesomeapp-ecc51.firebaseapp.com",
+  projectId: "awesomeapp-ecc51",
+  storageBucket: "awesomeapp-ecc51.appspot.com",
+  messagingSenderId: "625629138706",
+  appId: "1:625629138706:web:80cf702ddbeb8152ceadcb",
+  measurementId: "G-677NB1P5QR",
+};
---
+++ utils/AuthFirebase.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from "react";
+import * as firebaseui from "firebaseui";
+import "firebaseui/dist/firebaseui.css";
+import firebase from "firebase/compat/app";
+
+export default function AuthFirebase() {
+  useEffect(() => {
+    const ui =
+      firebaseui.auth.AuthUI.getInstance() ||
+      new firebaseui.auth.AuthUI(firebase.auth());
+    ui.start(".firebase-auth-container", {
+      signInOptions: [
+        {
+          provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+          requireDisplayName: false,
+        },
+      ],
+      signInSuccessUrl: "/authenticated",
+      privacyPolicyUrl: "/privacy-policy",
+    });
+  }, []);
+  return <div className="firebase-auth-container"></div>;
+}
