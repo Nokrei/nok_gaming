ea674b971968446ddc2ae598e739abd91e5986f5
Nok <dive877@gmail.com>
2022-12-01 20:32:31 +0400

    Remove unused imports

--- pages/authenticated.tsx
+++ pages/authenticated.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { onAuthStateChanged } from "firebase/auth";
 import axios from "axios";
 import { auth } from "../config/firebaseApp.config";
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
-import Head from "next/head";
 import dynamic from "next/dynamic";
-import firebase from "firebase/compat/app";
-import { firebaseConfig, auth } from "../config/firebaseApp.config";
+import { auth } from "../config/firebaseApp.config";
 
 // Needs to be a dynamic import, else will throw "ReferenceError: window is not defined".
 // Firebaseui expects a window object to be loaded, which it won't find due to SSR.
