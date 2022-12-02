89469875a8cf30f5bdbe691387d3b85c94e418a7
Nok <dive877@gmail.com>
2022-12-02 22:26:13 +0400

    Configure external image route, display games as cards

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "media.rawg.io",
+        pathname: "/media/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/authenticated.tsx
+++ pages/authenticated.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import Image from "next/image";
 import { useRouter } from "next/router";
 import { onAuthStateChanged } from "firebase/auth";
 import axios from "axios";
@@ -6,7 +7,7 @@ import { auth } from "../config/firebaseApp.config";
 import Layout from "../components/Layout/Layout";
 
 export default function Authenticated() {
-  const [genres, setGenres] = useState<any[]>([]);
+  const [games, setGames] = useState<any[]>([]);
 
   const router = useRouter();
 
@@ -19,27 +20,41 @@ export default function Authenticated() {
   });
 
   useEffect(() => {
-    const getGenres = async () => {
+    const getGames = async () => {
       try {
         const res = await axios({
           method: "GET",
-          url: "https://api.rawg.io/api/genres?key=567d69a8bf924ba1bebbf68419d9cd46",
+          url: "https://api.rawg.io/api/games?key=567d69a8bf924ba1bebbf68419d9cd46&page=1",
         });
-        setGenres(res.data.results);
-        console.log(res.data);
+        setGames(res.data.results);
+        console.log(res);
       } catch (err) {
         console.log(err);
       }
     };
-    getGenres();
+    getGames();
   }, []);
 
   return (
     <Layout>
-      <h1 className="text-3xl font-bold text-center">You are logged in</h1>
-      <div className="text-center">
-        {genres.map((genre) => {
-          return <p key={genre.id}>{genre.name}</p>;
+      <h1 className="text-3xl font-bold text-center mb-10">
+        You are logged in
+      </h1>
+      <div className=" grid grid-cols-5 justify-items-center">
+        {games.map((game) => {
+          return (
+            <div className="w-60 mb-5" key={game.id}>
+              <div className=" relative w-60 h-80">
+                <Image
+                  fill
+                  objectFit="cover"
+                  src={game.background_image}
+                  alt={`${game.name} poster`}
+                />
+              </div>
+              <p className=" text-white bg-black "> {game.name}</p>
+            </div>
+          );
         })}
       </div>
     </Layout>
