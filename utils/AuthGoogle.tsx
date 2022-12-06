6b5be6d2cd8b6c209468c75e7f5eed8e787817f5
Nok <dive877@gmail.com>
2022-12-06 15:20:58 +0400

    Add user to database on registration

--- utils/AuthGoogle.tsx
+++ utils/AuthGoogle.tsx
@@ -1,37 +1,31 @@
-import { useRouter } from "next/router";
-import { useContext } from "react";
 import {
-  getAuth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
 } from "firebase/auth";
-import AuthContext from "../context/AuthContext";
-import { auth } from "../config/firebaseApp.config";
+import { doc, setDoc } from "firebase/firestore";
+import { auth, db } from "../config/firebaseApp.config";
 
 export const CreateAccount = (email: string, password: string) => {
   createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
-      // Signed in
       const user = userCredential.user;
       console.log(user);
-
-      // ...
+      setDoc(doc(db, "users", user.uid), {
+        uid: user.uid,
+        email: user.email,
+      });
     })
     .catch((error) => {
       const errorCode = error.code;
       const errorMessage = error.message;
       console.log(error);
-
-      // ..
     });
 };
 
 export const LogIn = (email: string, password: string) => {
   signInWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
-      // Signed in
       const user = userCredential.user;
-      // ...
     })
     .catch((error) => {
       const errorCode = error.code;
