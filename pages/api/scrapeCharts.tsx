ef5b35337fbc05b5772e076ff3d9d445dc53b69d
Nok <dive877@gmail.com>
2023-04-03 20:54:00 +0400

    Try again with puppeteer

--- pages/api/scrapeCharts.tsx
+++ pages/api/scrapeCharts.tsx
@@ -1,69 +1,69 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-const { chromium: playwright } = require("playwright-core");
-const chromium = require("@sparticuz/chromium");
+// const { chromium: playwright } = require("playwright-core");
+// const chromium = require("@sparticuz/chromium");
 
-// const playwright = require('playwright-aws-lambda');
+// // const playwright = require('playwright-aws-lambda');
 
-const scrapeCharts = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === "POST") {
-    // Get title of game to scrape
-    // const gameTitle = req.body;
+// const scrapeCharts = async (req: NextApiRequest, res: NextApiResponse) => {
+//   if (req.method === "POST") {
+//     // Get title of game to scrape
+//     // const gameTitle = req.body;
 
-    // Open chromium browser
-    const browser = await playwright.launch({
-      args: chromium.args,
-      executablePath: await chromium.executablePath(),
-      headless: chromium.headless,
-    });
+//     // Open chromium browser
+//     const browser = await playwright.launch({
+//       args: chromium.args,
+//       executablePath: await chromium.executablePath(),
+//       headless: chromium.headless,
+//     });
 
-    // Open new page / tab in browser
-    const page = await browser.newPage();
+//     // Open new page / tab in browser
+//     const page = await browser.newPage();
 
-    // Navigate to the search results on steam charts using game title as search term
-    // await page.goto(`https://steamcharts.com/search/?q=${gameTitle}`);
+//     // Navigate to the search results on steam charts using game title as search term
+//     // await page.goto(`https://steamcharts.com/search/?q=${gameTitle}`);
 
-    try {
-      await page.goto("https://example.com");
-      const pageTitle = await page.title();
-      await browser.close();
-      // await page.click(".common-table a");
-      // const recentAndPeakData = await page
-      //   .locator(".app-stat .num")
-      //   .allInnerTexts();
+//     try {
+//       await page.goto("https://example.com");
+//       const pageTitle = await page.title();
+//       await browser.close();
+//       // await page.click(".common-table a");
+//       // const recentAndPeakData = await page
+//       //   .locator(".app-stat .num")
+//       //   .allInnerTexts();
 
-      // const monthlyAveragePlayers = await page
-      //   .locator(".odd .num-f")
-      //   .allInnerTexts();
+//       // const monthlyAveragePlayers = await page
+//       //   .locator(".odd .num-f")
+//       //   .allInnerTexts();
 
-      // const datesForAverageData = await page
-      //   .locator(".odd .month-cell")
-      //   .allInnerTexts();
+//       // const datesForAverageData = await page
+//       //   .locator(".odd .month-cell")
+//       //   .allInnerTexts();
 
-      // const monthlyAverageData = [];
-      // for (let i = 0; i < monthlyAveragePlayers.length; i++) {
-      //   monthlyAverageData.push({
-      //     average_players: monthlyAveragePlayers[i],
-      //     month: datesForAverageData[i],
-      //   });
-      // }
-      // monthlyAverageData.reverse();
+//       // const monthlyAverageData = [];
+//       // for (let i = 0; i < monthlyAveragePlayers.length; i++) {
+//       //   monthlyAverageData.push({
+//       //     average_players: monthlyAveragePlayers[i],
+//       //     month: datesForAverageData[i],
+//       //   });
+//       // }
+//       // monthlyAverageData.reverse();
 
-      // const scrapingData = {
-      //   recentAndPeakData,
-      //   monthlyAverageData,
-      // };
-      // res.status(200).json(scrapingData);
-      res.status(200).json(pageTitle);
-    } catch (error) {
-      console.log(error);
-      res.status(404);
-      // .json({ message: `Stats for title: ${gameTitle} not found` });
-    }
-  } else {
-    res.setHeader("Allow", ["POST"]);
-    res.status(405).json({ message: `Method ${req.method} not allowed` });
-  }
-};
+//       // const scrapingData = {
+//       //   recentAndPeakData,
+//       //   monthlyAverageData,
+//       // };
+//       // res.status(200).json(scrapingData);
+//       res.status(200).json(pageTitle);
+//     } catch (error) {
+//       console.log(error);
+//       res.status(404);
+//       // .json({ message: `Stats for title: ${gameTitle} not found` });
+//     }
+//   } else {
+//     res.setHeader("Allow", ["POST"]);
+//     res.status(405).json({ message: `Method ${req.method} not allowed` });
+//   }
+// };
 
 // import puppeteer from "puppeteer";
 // import puppeteer_core from "puppeteer-core";
@@ -147,61 +147,63 @@ const scrapeCharts = async (req: NextApiRequest, res: NextApiResponse) => {
 //   }
 // };
 
-// const puppeteer = require("puppeteer-core");
-// const chromium = require("@sparticuz/chromium");
+const puppeteer = require("puppeteer-core");
+const chromium = require("@sparticuz/chromium");
 
-// const scrapeCharts = async (req: NextApiRequest, res: NextApiResponse) => {
-//   if (req.method === "POST") {
-//     // Get title of game to scrape
-//     const gameTitle = req.body;
-//     // Open chromium browser
-//     const browser = await puppeteer.launch({
-//       args: chromium.args,
-//       defaultViewport: chromium.defaultViewport,
-//       executablePath: await chromium.executablePath(),
-//       headless: chromium.headless,
-//     });
-//     // Open new page / tab in browser
-//     const page = await browser.newPage();
-//     // Navigate to the search results on steam charts using game title as search term
-//     await page.goto(`https://steamcharts.com/search/?q=${gameTitle}`);
-//     try {
-//       await page.click(".common-table a");
-//       const recentAndPeakData = await page
-//         .locator(".app-stat .num")
-//         .allInnerTexts();
+const scrapeCharts = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "POST") {
+    // Get title of game to scrape
+    // const gameTitle = req.body;
+    // Open chromium browser
+    const browser = await puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath(),
+      headless: chromium.headless,
+    });
+    // Open new page / tab in browser
+    const page = await browser.newPage();
+    // Navigate to the search results on steam charts using game title as search term
+    // await page.goto(`https://steamcharts.com/search/?q=${gameTitle}`);
+    await page.goto("https://example.com");
+    try {
+      const pageTitle = await page.title();
+      await browser.close();
+      // await page.click(".common-table a");
+      // const recentAndPeakData = await page
+      //   .locator(".app-stat .num")
+      //   .allInnerTexts();
 
-//       const monthlyAveragePlayers = await page
-//         .locator(".odd .num-f")
-//         .allInnerTexts();
+      // const monthlyAveragePlayers = await page
+      //   .locator(".odd .num-f")
+      //   .allInnerTexts();
 
-//       const datesForAverageData = await page
-//         .locator(".odd .month-cell")
-//         .allInnerTexts();
+      // const datesForAverageData = await page
+      //   .locator(".odd .month-cell")
+      //   .allInnerTexts();
 
-//       const monthlyAverageData = [];
-//       for (let i = 0; i < monthlyAveragePlayers.length; i++) {
-//         monthlyAverageData.push({
-//           average_players: monthlyAveragePlayers[i],
-//           month: datesForAverageData[i],
-//         });
-//       }
-//       monthlyAverageData.reverse();
+      // const monthlyAverageData = [];
+      // for (let i = 0; i < monthlyAveragePlayers.length; i++) {
+      //   monthlyAverageData.push({
+      //     average_players: monthlyAveragePlayers[i],
+      //     month: datesForAverageData[i],
+      //   });
+      // }
+      // monthlyAverageData.reverse();
 
-//       const scrapingData = {
-//         recentAndPeakData,
-//         monthlyAverageData,
-//       };
-//       res.status(200).json(scrapingData);
-//     } catch (error) {
-//       console.log(error);
-//       res
-//         .status(404)
-//         .json({ message: `Stats for title: ${gameTitle} not found` });
-//     }
-//   } else {
-//     res.setHeader("Allow", ["POST"]);
-//     res.status(405).json({ message: `Method ${req.method} not allowed` });
-//   }
-// };
+      // const scrapingData = {
+      //   recentAndPeakData,
+      //   monthlyAverageData,
+      // };
+      res.status(200).json(pageTitle);
+    } catch (error) {
+      console.log(error);
+      res.status(404);
+      // .json({ message: `Stats for title: ${gameTitle} not found` });
+    }
+  } else {
+    res.setHeader("Allow", ["POST"]);
+    res.status(405).json({ message: `Method ${req.method} not allowed` });
+  }
+};
 export default scrapeCharts;
